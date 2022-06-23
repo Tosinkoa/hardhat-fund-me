31f6f481a4b41f8b81add037c3c1f1603b2ab95c
Ojo Paul <tosinkoa1@gmail.com>
2022-06-23 07:05:24 +0100

    Solving bugs

--- scripts/test.test.js
+++ scripts/test.test.js
@@ -5,7 +5,6 @@ describe("PaulFundMe", async () => {
   let paulFundMe, deployer, mockV3Aggregator;
 
   beforeEach(async () => {
-    // const accounts = await getSigners()  This is use for getting private keys
     deployer = (await getNamedAccounts()).deployer;
     await deployments.fixture(["all"]);
     paulFundMe = await ethers.getContract("PaulFundMe", deployer);
@@ -17,12 +16,6 @@ describe("PaulFundMe", async () => {
     it("sets the aggregator addesses correctly", async () => {
       const response = await paulFundMe.priceFeed();
       assert.equal(response, mockV3Aggregator.address);
-    });
-  });
-
-  describe("fund", async () => {
-    it("Fails if you don't send enough ETH", async () => {
-      await paulFundMe.fund();
     });
   });
 });
